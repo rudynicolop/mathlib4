bee8a38038790bee3c1b58c184b37422f3d9587e
rudynicolop <rudypotamia@gmail.com>
2025-03-05 11:30:04 +0100

    Weighted Languages and Semiring proofs

--- Mathlib.lean
+++ Mathlib.lean
@@ -3282,6 +3282,7 @@ public import Mathlib.Computability.TMToPartrec
 public import Mathlib.Computability.Tape
 public import Mathlib.Computability.TuringDegree
 public import Mathlib.Computability.TuringMachine
+public import Mathlib.Computability.WeightedLanguage
 public import Mathlib.Condensed.AB
 public import Mathlib.Condensed.Basic
 public import Mathlib.Condensed.CartesianClosed
