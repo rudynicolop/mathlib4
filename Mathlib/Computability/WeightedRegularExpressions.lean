a45f5f56dc3f52f178f396b693cffad0e82b7e48
rudynicolop <rudypotamia@gmail.com>
2025-10-13 18:25:31 -0700

    derivative skeleton attempt: probably needs adjustment

--- Mathlib/Computability/WeightedRegularExpressions.lean
+++ Mathlib/Computability/WeightedRegularExpressions.lean
@@ -79,7 +79,7 @@ theorem comp_def (P Q : WRegExp α κ) : comp P Q = P * Q :=
 
 section matches'
 
-variable [W : Semiring κ] [DecidableEq α]
+variable [DecidableEq α] [W : Semiring κ]
 
 /-- `macthes' P` provides a *weighted* language for all strings that `P` macthes.
 
@@ -93,6 +93,73 @@ def matches' : WRegExp α κ → WeightedLanguage α κ
   | weight w => WeightedLanguage.scalar_prodl w 1
   | P + Q => P.matches' + Q.matches'
   | P * Q => P.matches' * Q.matches'
+
+theorem matches'_zero : (0 : WRegExp α κ).matches' = 0 :=
+  rfl
+
+theorem matches'_epsilon : (1 : WRegExp α κ).matches' = 1 :=
+  rfl
+
+theorem matches'_char (a : α) : (char a).matches' = (fun x ↦ if x = [a] then W.one else 0) :=
+  rfl
+
+theorem matches'_add (P Q : WRegExp α κ) : (P + Q).matches' = P.matches' + Q.matches' :=
+  rfl
+
+theorem matches'_mul (P Q : WRegExp α κ) : (P * Q).matches' = P.matches' * Q.matches' :=
+  rfl
+
+@[simp]
+theorem matches'_pow (P : WRegExp α κ) (n : ℕ) : (P ^ n).matches' = P.matches' ^ n := by
+  induction n generalizing P
+  case zero => rfl
+  case succ n ih =>
+    simp [pow_succ, ←ih]
+    rfl
+
+/-- `matchEpsilon P` is true if and only if `P` matches the empty string -/
+def matchEpsilon : WRegExp α κ → κ
+  | 0 => 1
+  | 1 => 0
+  | char _ => 0
+  | weight w => w
+  | P + Q => P.matchEpsilon + Q.matchEpsilon
+  | P * Q => P.matchEpsilon * Q.matchEpsilon
+
+/-- `P.deriv a` matches `x` if `P` matches `a :: x`, the Brzozowski derivative of `P` with respect
+  to `a` -/
+def deriv : WRegExp α κ → α → WRegExp α κ
+  | 0, _ => 0
+  | 1, _ => 0
+  | weight _, _ => 0
+  | char a₁, a₂ => if a₁ = a₂ then 1 else 0
+  | P + Q, a => deriv P a + deriv Q a
+  | P * Q, a => deriv P a * Q + weight P.matchEpsilon * deriv Q a
+
+@[simp]
+theorem deriv_zero (a : α) : deriv 0 a = (0 : WRegExp α κ) :=
+  rfl
+
+@[simp]
+theorem deriv_one (a : α) : deriv 1 a = (0 : WRegExp α κ) :=
+  rfl
+
+@[simp]
+theorem deriv_char_self (a : α) : deriv (char a) a = (1 : WRegExp α κ) :=
+  if_pos rfl
+
+@[simp]
+theorem deriv_char_of_ne (a b : α) (h : a ≠ b) : deriv (char a) b = (0 : WRegExp α κ) :=
+  if_neg h
+
+@[simp]
+theorem deriv_add (P Q : WRegExp α κ) (a : α) : deriv (P + Q) a = deriv P a + deriv Q a :=
+  rfl
+
+-- TODO: does this actually compute the weight?
+def rmatch : WRegExp α κ → List α → κ
+  | P, [] => matchEpsilon P
+  | P, a :: as => rmatch (P.deriv a) as
 
 end matches'
 
