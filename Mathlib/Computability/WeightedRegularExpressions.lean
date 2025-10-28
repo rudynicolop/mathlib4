b67181dc0c534a06b42576db681d59fc9fd2de64
rudynicolop <rudypotamia@gmail.com>
2025-10-28 14:10:19 -0700

    WeightedRegex: fix file author (split from bd11ec93f8d)

--- Mathlib/Computability/WeightedRegularExpressions.lean
+++ Mathlib/Computability/WeightedRegularExpressions.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Rudy Peterson. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Fox Thomson
+Authors: Rudy Peterson
 -/
 import Mathlib.Computability.WeightedLanguage
 
